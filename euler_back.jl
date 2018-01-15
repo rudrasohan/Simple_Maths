905e39397514d497b58374966c808b7af863584e
Sohan Rudra <sohanrudra@gmail.com>
2018-01-15 23:16:31 +0530

    Fixed fn names

--- euler_back.jl
+++ euler_back.jl
@@ -4,7 +4,7 @@ function f(x::Float64,y::Float64) ::Float64
 	return (x^2+y)    
 end
 
-function forward_delta(a::Float64,b::Float64,η::Float64,a0::Float64)
+function backward_delta(a::Float64,b::Float64,η::Float64,a0::Float64)
 	b = b - η
 	T = collect(a:η:b)
 	y1 = 0.0
@@ -26,4 +26,4 @@ a = 0.0
 y_a = 1.0
 ℏ = 0.00001
 x = 4.0
-forward_delta(a,x,ℏ,y_a)
+backward_delta(a,x,ℏ,y_a)
